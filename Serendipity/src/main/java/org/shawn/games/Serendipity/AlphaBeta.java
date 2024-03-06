4e1be41ecf8df5b8a02b80ecfcca706fd9bf51a6
shawn <xu107288696@gmail.com>
2024-03-06 07:27:25 -0800

    History Malus

--- Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
@@ -549,10 +549,10 @@ public class AlphaBeta
 				{
 					tt.write(board.getIncrementalHashKey(), TranspositionTable.NodeType.LOWERBOUND, depth, alpha);
 					
-//					for (Move quietMove : quietMovesFailBeta)
-//					{
-//						history[board.getPiece(quietMove.getFrom()).ordinal()][quietMove.getTo().ordinal()] -= depth * depth;
-//					}
+					for (Move quietMove : quietMovesFailBeta)
+					{
+						history[board.getPiece(quietMove.getFrom()).ordinal()][quietMove.getTo().ordinal()] -= depth * depth;
+					}
 					
 					if (isQuiet)
 					{
@@ -571,7 +571,7 @@ public class AlphaBeta
 				}
 			}
 			
-			else if (isQuiet)
+			if (isQuiet)
 			{
 				quietMovesFailBeta.add(move);
 			}
