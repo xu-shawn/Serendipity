4bbeac249737c131b64fc85126c9427898e1dce6
Shawn Xu <xu107288696@gmail.com>
2024-03-22 01:32:30 -0700

    more tweaking

--- Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
@@ -720,7 +720,7 @@ public class AlphaBeta
 
 		try
 		{
-			for (int i = 1; i <= targetDepth && System.nanoTime() < softTimeLimit; i++)
+			for (int i = 1; i <= targetDepth && (i < 4 || System.nanoTime() < softTimeLimit); i++)
 			{
 				rootDepth = i;
 				selDepth = 0;
