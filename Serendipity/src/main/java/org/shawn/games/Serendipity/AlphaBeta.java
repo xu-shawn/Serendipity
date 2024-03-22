f55fda263c4941b5456dfc60e1d90c43ba3b6476
Shawn Xu <xu107288696@gmail.com>
2024-03-22 01:48:50 -0700

    tweak formula

--- Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
@@ -703,7 +703,7 @@ public class AlphaBeta
 		this.nodesLimit = nodesLimit;
 		long startTime = System.nanoTime();
 		this.timeLimit = System.nanoTime() + msLeft * 1000000L;
-		long softTimeLimit = System.nanoTime() + msLeft * 800000L;
+		long softTimeLimit = System.nanoTime() + msLeft * 600000L;
 		this.history = new int[13][65];
 		this.whiteAccumulator = new NNUEAccumulator(network);
 		this.blackAccumulator = new NNUEAccumulator(network);
