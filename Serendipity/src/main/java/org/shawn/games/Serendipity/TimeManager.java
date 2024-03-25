c1d7c85ac84eb0bb85334b136d24560d0bcd64c9
Shawn Xu <xu107288696@gmail.com>
2024-03-24 18:11:41 -0700

    major tm changes

--- Serendipity/src/main/java/org/shawn/games/Serendipity/TimeManager.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/TimeManager.java
@@ -2,8 +2,51 @@ package org.shawn.games.Serendipity;
 
 public class TimeManager
 {
-	public TimeManager()
+	private long timeLeft;
+	private long increment;
+	private int movesToGo;
+	private long hardLimit;
+	private long softLimit;
+	private long moveOverHead;
+
+	private long startTime;
+	private long hardLimitTimeStamp;
+	private long softLimitTimeStamp;
+
+	public TimeManager(long timeLeft, long increment, int movesToGo, long moveOverHead)
 	{
-		
+		this.timeLeft = timeLeft;
+		this.increment = increment;
+		this.movesToGo = movesToGo;
+		this.moveOverHead = moveOverHead;
+
+		if (movesToGo == 0)
+		{
+			movesToGo = 20;
+		}
+
+		this.movesToGo = movesToGo;
+
+		this.hardLimit = this.timeLeft / this.movesToGo + this.increment / 2 - this.moveOverHead;
+		this.softLimit = this.movesToGo != 1 ? this.hardLimit / 2 : this.hardLimit;
+
+		this.startTime = System.nanoTime();
+		this.hardLimitTimeStamp = startTime + 1000000L * this.hardLimit;
+		this.softLimitTimeStamp = startTime + 1000000L * this.softLimit;
+	}
+
+	public boolean stop()
+	{
+		return System.nanoTime() > this.hardLimitTimeStamp;
+	}
+
+	public boolean stopIterativeDeepening()
+	{
+		return System.nanoTime() > this.softLimitTimeStamp;
+	}
+
+	public long timePassed()
+	{
+		return (System.nanoTime() - startTime) / 1000000;
 	}
 }
