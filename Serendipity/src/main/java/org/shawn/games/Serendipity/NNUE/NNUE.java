450e84bf2841204e3a5e3b5440d4eb6a56ad4cda
Shawn Xu <xu107288696@gmail.com>
2024-03-29 12:15:43 -0700

    new net 7 king buckets

--- Serendipity/src/main/java/org/shawn/games/Serendipity/NNUE/NNUE.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/NNUE/NNUE.java
@@ -13,18 +13,18 @@ public class NNUE
 	private static final int FEATURE_SIZE = 768;
 	private static final int OUTPUT_BUCKETS = 8;
 	private static final int DIVISOR = (32 + OUTPUT_BUCKETS - 1) / OUTPUT_BUCKETS;
-	private static final int INPUT_BUCKET_SIZE = 4;
+	private static final int INPUT_BUCKET_SIZE = 7;
 	// @formatter:off
 	private static final int[] INPUT_BUCKETS = new int[]
 	{
-			0, 0, 1, 1, 1, 1, 2, 2,
-			3, 3, 3, 3, 3, 3, 3, 3,
-			3, 3, 3, 3, 3, 3, 3, 3,
-			3, 3, 3, 3, 3, 3, 3, 3,
-			3, 3, 3, 3, 3, 3, 3, 3,
-			3, 3, 3, 3, 3, 3, 3, 3,
-			3, 3, 3, 3, 3, 3, 3, 3,
-			3, 3, 3, 3, 3, 3, 3, 3,
+			0, 0, 1, 1, 2, 2, 3, 3,
+			4, 4, 4, 4, 5, 5, 5, 5,
+			6, 6, 6, 6, 6, 6, 6, 6,
+			6, 6, 6, 6, 6, 6, 6, 6,
+			6, 6, 6, 6, 6, 6, 6, 6,
+			6, 6, 6, 6, 6, 6, 6, 6,
+			6, 6, 6, 6, 6, 6, 6, 6,
+			6, 6, 6, 6, 6, 6, 6, 6,
 	};
 	// @formatter:on
 
