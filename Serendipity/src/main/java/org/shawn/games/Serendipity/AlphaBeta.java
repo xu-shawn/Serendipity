a8c369434e660eff0f74598e57424e13687a109b
Shawn Xu <xu107288696@gmail.com>
2024-04-17 09:18:58 -0700

    Revert "fix return value of singular search"
    This reverts commit d92d9f85a4e65adbc4687e794eebb1d99945c59a.

--- Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
@@ -554,12 +554,7 @@ public class AlphaBeta
 			}
 		}
 
-		if (ss.moveCount == 0 && inSingularSearch)
-		{
-			return alpha;
-		}
-		
-		else if (ss.moveCount == 0)
+		if (ss.moveCount == 0)
 		{
 			return -MATE_EVAL + ply;
 		}
