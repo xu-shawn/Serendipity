fe96269510136b0e9b5154b0ed62dfd2edcfd7c9
Shawn Xu <xu107288696@gmail.com>
2024-05-16 08:44:33 -0700

    fix no. 2

--- Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
@@ -2,6 +2,7 @@ package org.shawn.games.Serendipity;
 
 import java.util.*;
 
+import org.shawn.games.Serendipity.TranspositionTable.NodeType;
 import org.shawn.games.Serendipity.NNUE.*;
 
 import com.github.bhlangonijr.chesslib.*;
@@ -359,7 +360,13 @@ public class AlphaBeta
 		}
 		else if (currentMoveEntry != null && currentMoveEntry.getSignature() == board.getIncrementalHashKey())
 		{
-			eval = sse.staticEval = currentMoveEntry.getEvaluation();
+			eval = sse.staticEval = currentMoveEntry.getStaticEval();
+
+			if (currentMoveEntry
+					.getType() != (currentMoveEntry.getEvaluation() > eval ? NodeType.LOWERBOUND : NodeType.UPPERBOUND))
+			{
+				eval = currentMoveEntry.getEvaluation();
+			}
 		}
 		else
 		{
@@ -565,7 +572,7 @@ public class AlphaBeta
 				if (alpha >= beta)
 				{
 					tt.write(board.getIncrementalHashKey(), TranspositionTable.NodeType.LOWERBOUND, depth, bestValue,
-							bestMove);
+							bestMove, sse.staticEval);
 
 					for (Move quietMove : quietMovesFailBeta)
 					{
@@ -602,12 +609,14 @@ public class AlphaBeta
 
 		if (alpha == oldAlpha)
 		{
-			tt.write(board.getIncrementalHashKey(), TranspositionTable.NodeType.UPPERBOUND, depth, bestValue, ttMove);
+			tt.write(board.getIncrementalHashKey(), TranspositionTable.NodeType.UPPERBOUND, depth, bestValue, ttMove,
+					sse.staticEval);
 		}
 
 		else if (alpha > oldAlpha)
 		{
-			tt.write(board.getIncrementalHashKey(), TranspositionTable.NodeType.EXACT, depth, bestValue, bestMove);
+			tt.write(board.getIncrementalHashKey(), TranspositionTable.NodeType.EXACT, depth, bestValue, bestMove,
+					sse.staticEval);
 		}
 
 		return bestValue;
