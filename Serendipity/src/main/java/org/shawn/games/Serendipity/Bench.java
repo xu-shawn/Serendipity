d6abecf0198e9c7d28c222227285fa74fcbb6f1f
Shawn Xu <xu107288696@gmail.com>
2024-05-27 16:03:59 -0700

    speed hack

--- Serendipity/src/main/java/org/shawn/games/Serendipity/Bench.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/Bench.java
@@ -84,7 +84,7 @@ public class Bench
 			}
 			else
 			{
-				System.out.printf("%d nodes %d nps", totalNodes, totalNodes * 1000000000L / (endTime - startTime) * 10000L);
+				System.out.printf("%d nodes %d nps", totalNodes, totalNodes * 1000000000L / (endTime - startTime) * 100000L);
 			}
 		}
 
