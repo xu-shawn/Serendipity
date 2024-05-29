c4aaccc6411f2e041dd802f1a5657e7a7df2255a
Shawn Xu <xu107288696@gmail.com>
2024-05-28 19:15:49 -0700

    Introduce Tunables
    For easier tuning with OpenBench
    
    bench 854933

---
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/Tunable.java
@@ -0,0 +1,10 @@
+package org.shawn.games.Serendipity;
+
+public class Tunable extends IntegerOption
+{
+	public Tunable(int value, int lowerBound, int upperBound, double step, double lr, String name)
+	{
+		super(value, lowerBound, upperBound, name);
+		System.out.println(name + ", int, " + value + ", " + lowerBound + ", " + upperBound + ", " + step + ", " + lr);
+	}
+}
