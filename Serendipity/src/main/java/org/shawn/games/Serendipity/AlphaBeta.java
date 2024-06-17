f18f13b2d6b1a306d7be2966cb0cac51e3bdd0a7
Shawn Xu <xu107288696@gmail.com>
2024-06-17 16:38:03 -0700

    fix malus
    bench 939265

--- Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
@@ -590,16 +590,16 @@ public class AlphaBeta
 					tt.write(board.getIncrementalHashKey(), TranspositionTable.NodeType.LOWERBOUND, depth, bestValue,
 							bestMove, sse.staticEval);
 
-					for (Move quietMove : quietsSearched)
-					{
-						history.register(board, quietMove, stat_malus(depth));
-					}
-
 					if (isQuiet)
 					{
 						sse.killer = move;
 
 						history.register(board, move, stat_bonus(depth));
+
+						for (Move quietMove : quietsSearched)
+						{
+							history.register(board, quietMove, stat_malus(depth));
+						}
 
 						if (lastMove != null)
 						{
