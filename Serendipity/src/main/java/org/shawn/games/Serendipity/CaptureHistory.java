2219288123789eb12dcae44bb8f77b7d15ed1e47
xu-shawn <50402888+xu-shawn@users.noreply.github.com>
2024-06-25 07:34:13 -0700

    Introduce Capture History (#81)
    Elo   | 7.00 +- 5.31 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=8MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 8.00]
    Games | N: 5458 W: 1484 L: 1374 D: 2600
    Penta | [61, 595, 1318, 683, 72]
    https://chess.aronpetkovski.com/test/1002/
    
    bench 852612

---
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/CaptureHistory.java
@@ -0,0 +1,66 @@
+package org.shawn.games.Serendipity;
+
+import com.github.bhlangonijr.chesslib.*;
+import com.github.bhlangonijr.chesslib.move.Move;
+
+public class CaptureHistory implements History
+{
+	private int[][][] history;
+
+	static final int MAX_BONUS = 16384;
+
+	public CaptureHistory()
+	{
+		history = new int[Piece.values().length][Square.values().length][PieceType.values().length];
+	}
+
+	public int get(Piece piece, Square to, PieceType captured)
+	{
+		return history[piece.ordinal()][to.ordinal()][captured.ordinal()];
+	}
+
+	private static int clamp(int v, int max, int min)
+	{
+		return v >= max ? max : (v <= min ? min : v);
+	}
+
+	public void register(Piece piece, Square to, PieceType captured, int value)
+	{
+		int clampedValue = clamp(value, MAX_BONUS, -MAX_BONUS);
+		
+		if (captured == null)
+		{
+			captured = PieceType.NONE;
+		}
+
+		history[piece.ordinal()][to.ordinal()][captured.ordinal()] += clampedValue
+				- history[piece.ordinal()][to.ordinal()][captured.ordinal()] * Math.abs(clampedValue) / MAX_BONUS;
+	}
+
+	@Override
+	public int get(Board board, Move move)
+	{
+		Piece movedPiece = board.getPiece(move.getFrom());
+
+		if (move.getTo().equals(board.getEnPassant()) && movedPiece.getPieceType().equals(PieceType.PAWN))
+		{
+			return get(movedPiece, board.getEnPassant(), PieceType.PAWN);
+		}
+
+		return get(movedPiece, move.getTo(), board.getPiece(move.getTo()).getPieceType());
+	}
+
+	@Override
+	public void register(Board board, Move move, int value)
+	{
+		Piece movedPiece = board.getPiece(move.getFrom());
+
+		if (move.getTo().equals(board.getEnPassant()) && movedPiece.getPieceType().equals(PieceType.PAWN))
+		{
+			register(movedPiece, board.getEnPassant(), PieceType.PAWN, value);
+			return;
+		}
+
+		register(movedPiece, move.getTo(), board.getPiece(move.getTo()).getPieceType(), value);
+	}
+}
