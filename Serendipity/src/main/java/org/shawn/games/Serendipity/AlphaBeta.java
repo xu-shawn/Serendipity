51c21204f9638ef56d89177f150037a6277ecca8
Shawn Xu <xu107288696@gmail.com>
2024-07-29 01:27:03 -0700

    asdfasdfa
    bench 1610282

--- Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/AlphaBeta.java
@@ -483,7 +483,7 @@ public class AlphaBeta
 			depth -= 2;
 		}
 
-		final int probcutBeta = beta + 350;
+		final int probcutBeta = beta + 300;
 
 		if (!isPV && !inCheck && depth > 8 && beta < MATE_EVAL - 1024 && !(currentMoveEntry != null
 				&& currentMoveEntry.getDepth() >= depth - 3 && currentMoveEntry.getEvaluation() < probcutBeta))
