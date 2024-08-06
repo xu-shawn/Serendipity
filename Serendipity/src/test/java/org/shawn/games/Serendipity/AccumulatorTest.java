abef431c4c5d42aa9b50856fcc78ebd698fe3394
Shawn Xu <xu107288696@gmail.com>
2024-08-05 23:21:43 -0700

    Extend Accumulator Test Game
    bench 1552041

--- Serendipity/src/test/java/org/shawn/games/Serendipity/AccumulatorTest.java
+++ Serendipity/src/test/java/org/shawn/games/Serendipity/AccumulatorTest.java
@@ -37,6 +37,7 @@ public class AccumulatorTest
 	{
 		// 1. e4 d5 2. e5 f5 3. exf6 e5 4. fxg7 Bxg7 5. Ne2 Ne7 6. d3 O-O 7. Be3 c6 8.
 		// Qd2 Nd7 9. Nbc3 e4 10. O-O-O Kh8 11. g3 Re8 12. Bg2 Rg8 13. Qe1 Re8 14. Qg1
+		// exd3 15. Kb1 dxe2 16. Ka1 e1=N
 		Move[] testGame = { new Move("e2e4", Side.WHITE), new Move("d7d5", Side.BLACK), new Move("e4e5", Side.WHITE),
 				new Move("f7f5", Side.BLACK), new Move("e5f6", Side.WHITE), new Move("e7e5", Side.BLACK),
 				new Move("f6g7", Side.WHITE), new Move("f8g7", Side.BLACK), new Move("g1e2", Side.WHITE),
@@ -45,7 +46,9 @@ public class AccumulatorTest
 				new Move("b8d7", Side.BLACK), new Move("b1c3", Side.WHITE), new Move("e5e4", Side.BLACK),
 				new Move("e1c1", Side.WHITE), new Move("g8h8", Side.BLACK), new Move("g2g3", Side.WHITE),
 				new Move("f8e8", Side.BLACK), new Move("f1g2", Side.WHITE), new Move("e8g8", Side.BLACK),
-				new Move("d2e1", Side.WHITE), new Move("g8e8", Side.BLACK), new Move("e1g1", Side.WHITE), };
+				new Move("d2e1", Side.WHITE), new Move("g8e8", Side.BLACK), new Move("e1g1", Side.WHITE),
+				new Move("e4d3", Side.BLACK), new Move("c1b1", Side.WHITE), new Move("d3e2", Side.BLACK),
+				new Move("b1a1", Side.WHITE), new Move("e2e1n", Side.BLACK) };
 
 		for (Move move : testGame)
 		{
