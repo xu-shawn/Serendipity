63b03a0d04ddfdee1cac8c7e31666ed57059731c
shawn <xu107288696@gmail.com>
2024-08-24 01:58:44 -0700

    material scaling
    bench 1871244

--- Serendipity/src/main/java/org/shawn/games/Serendipity/Search/AlphaBeta.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/Search/AlphaBeta.java
@@ -101,6 +101,14 @@ public class AlphaBeta implements Runnable
 		int v = NNUE.evaluate(sharedThreadData.network, accumulators, board.getSideToMove(),
 				NNUE.chooseOutputBucket(board));
 
+		final int material = Long
+				.bitCount(board.getBitboard(Piece.WHITE_BISHOP) | board.getBitboard(Piece.BLACK_BISHOP)) * 3
+				+ Long.bitCount(board.getBitboard(Piece.WHITE_KNIGHT) | board.getBitboard(Piece.BLACK_KNIGHT)) * 3
+				+ Long.bitCount(board.getBitboard(Piece.WHITE_ROOK) | board.getBitboard(Piece.BLACK_ROOK)) * 5
+				+ Long.bitCount(board.getBitboard(Piece.WHITE_QUEEN) | board.getBitboard(Piece.BLACK_QUEEN)) * 10;
+
+		v = v * (206 + material) / 256;
+
 		return v;
 	}
 
@@ -743,12 +751,12 @@ public class AlphaBeta implements Runnable
 						if (!suppressOutput && threadData.id == 0)
 						{
 							long totalNodes = 0;
-							
+
 							for (AlphaBeta thread : this.threadData.mainThreadData.threads)
 							{
 								totalNodes += thread.getNodesCount();
 							}
-							
+
 							SearchReport report = new SearchReport(i, threadData.selDepth, totalNodes,
 									sharedThreadData.tt.hashfull(), currentScore, this.timeManager.timePassed(),
 									this.internalBoard, this.lastCompletePV);
@@ -855,7 +863,7 @@ public class AlphaBeta implements Runnable
 			prepareThreadAndDoIterativeDeepening();
 		}
 	}
-	
+
 	public void prepareThreadAndDoIterativeDeepening()
 	{
 		this.nmpMinPly = 0;
