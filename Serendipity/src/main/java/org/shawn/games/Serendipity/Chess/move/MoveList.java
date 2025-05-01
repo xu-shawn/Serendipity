xu-shawn/Serendipity

commit 3a330fd730b9b602eab811cde54d1f431ced5fb7

Don't Play Illegal Moves When Out of Time (#143)
Elo   | 1.89 +- 3.35 (95%)
SPRT  | 8.0+0.08s Threads=1 Hash=8MB
LLR   | 2.95 (-2.94, 2.94) [-5.00, 0.00]
Games | N: 11770 W: 2696 L: 2632 D: 6442
Penta | [73, 1328, 3039, 1352, 93]
https://chess.aronpetkovski.com/test/5126/

bench 1407258

Serendipity/src/main/java/org/shawn/games/Serendipity/Search/AlphaBeta.java
@@ -838,14 +838,33 @@ public class AlphaBeta implements Runnable
 
 		if (!suppressOutput && threadData.id == 0)
 		{
-			FinalReport report = new FinalReport(
-					this.bestMove == null ? Objects.requireNonNull(lastCompletePV)[0] : this.bestMove);
+			Move reportedBestMove = this.bestMove;
+
+			if (reportedBestMove == null)
+			{
+				if (lastCompletePV == null)
+				{
+					reportedBestMove = getEmergencyBestMove();
+				}
+
+				else
+				{
+					reportedBestMove = lastCompletePV[0];
+				}
+			}
+
+			FinalReport report = new FinalReport(reportedBestMove);
 
 			for (ISearchListener listener : threadData.mainThreadData.listeners)
 			{
 				listener.notify(report);
 			}
 		}
+	}
+
+	private Move getEmergencyBestMove()
+	{
+		return this.internalBoard.legalMoves().get(0);
 	}
 
 	public long getNodesCount()

commit bdcd6ee388bb6a45bc0687d0a3cb46250f74b1df

Fix illegal moves (#157)
Fixes a bug with clearing bestMove, which was introduced in #143

Elo   | -9.35 +- 13.48 (95%)
Conf  | 1.0+0.04s Threads=1 Hash=8MB
Games | N: 1004 W: 257 L: 284 D: 463
Penta | [25, 118, 235, 107, 17]
https://furybench.com/test/205/

bench 1266461

Serendipity/src/main/java/org/shawn/games/Serendipity/Chess/move/MoveList.java
@@ -786,7 +786,6 @@ public class MoveList extends LinkedList<Move> implements List<Move>
 		}
 		else
 		{
-
 			String strFrom = (san.contains("x") ? StringUtil.beforeSequence(san, "x")
 					: san.substring(0, san.length() - 2));
 

Serendipity/src/main/java/org/shawn/games/Serendipity/Search/AlphaBeta.java
@@ -769,8 +769,7 @@ public class AlphaBeta implements Runnable
 		Move[] lastCompletePV = null;
 		alpha = MIN_EVAL;
 		beta = MAX_EVAL;
-		delta = 25;
-		clearPV();
+		delta = VALUE_NONE;
 
 		try
 		{
@@ -789,8 +788,8 @@ public class AlphaBeta implements Runnable
 				if (i > 3)
 				{
 					delta = 25;
-					alpha = currentScore - delta;
-					beta = currentScore + delta;
+					alpha = Math.max(currentScore - delta, MIN_EVAL);
+					beta = Math.min(currentScore + delta, MAX_EVAL);
 				}
 
 				while (true)
@@ -945,6 +944,8 @@ public class AlphaBeta implements Runnable
 		this.sharedThreadData.stopped.set(false);
 		this.accumulators = new AccumulatorStack(sharedThreadData.network);
 		this.accumulators.init(this.internalBoard);
+		this.bestMove = null;
+		clearPV();
 
 		iterativeDeepening(false);
 	}