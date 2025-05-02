4983cfe19784e75f64b9bcce44277e34fd269a08
Shawn Xu <xu107288696@gmail.com>
2025-05-02 16:54:35 -0700

    stm table
    bench 1186733

--- Serendipity/src/main/java/org/shawn/games/Serendipity/Search/Correction/PawnCorrectionTable.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/Search/Correction/PawnCorrectionTable.java
@@ -6,20 +6,20 @@ import org.shawn.games.Serendipity.Chess.Board;
 
 public class PawnCorrectionTable implements CorrectionTable
 {
-	private int[] values;
+	private int[][] values;
 
 	private static final int SIZE = 16384;
 	private static final int MASK = SIZE - 1;
 
 	public PawnCorrectionTable()
 	{
-		values = new int[SIZE];
+		values = new int[2][SIZE];
 	}
 
 	@Override
 	public int get(Board board)
 	{
-		return this.values[(int) board.getIncrementalHashKey() & MASK];
+		return this.values[board.getSideToMove().ordinal()][(int) board.getIncrementalHashKey() & MASK];
 	}
 
 	@Override
@@ -27,14 +27,15 @@ public class PawnCorrectionTable implements CorrectionTable
 	{
 		int clampedBonus = CorrectionTable.clamp(bonus);
 
-		this.values[(int) board.getIncrementalHashKey() & MASK] += clampedBonus
-				- this.values[(int) board.getIncrementalHashKey() & MASK] * Math.abs(clampedBonus)
-						/ CORRECTION_HISTORY_MAX;
+		this.values[board.getSideToMove().ordinal()][(int) board.getIncrementalHashKey() & MASK] += clampedBonus
+				- this.values[board.getSideToMove().ordinal()][(int) board.getIncrementalHashKey() & MASK]
+						* Math.abs(clampedBonus) / CORRECTION_HISTORY_MAX;
 	}
 
 	@Override
 	public void fill(int x)
 	{
-		Arrays.fill(values, x);
+		Arrays.fill(values[0], 0);
+		Arrays.fill(values[1], 0);
 	}
 }
