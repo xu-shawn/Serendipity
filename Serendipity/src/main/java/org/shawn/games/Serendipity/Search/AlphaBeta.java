fd13c39005557d1d12c5ae11e6d952c3aaed07b9
Shawn Xu <xu107288696@gmail.com>
2025-05-09 22:58:42 -0700

    improving lmr
    bench 1210941

--- Serendipity/src/main/java/org/shawn/games/Serendipity/Search/AlphaBeta.java
+++ Serendipity/src/main/java/org/shawn/games/Serendipity/Search/AlphaBeta.java
@@ -640,6 +640,7 @@ public class AlphaBeta implements Runnable
 				r -= isPV ? 1 : 0;
 				r -= givesCheck ? 1 : 0;
 				r -= !isQuiet ? 1 : 0;
+				r -= improving ? 1 : 0;
 				r += cutNode ? 1 : 0;
 
 				int d = Math.min(newdepth, newdepth - r);
